aa09b4dbaab47a19e542228adf0160effcd1cc1e
Volodymyr Kurov <vokur13@gmail.com>
2022-12-02 16:57:04 +0200

    deleteItem resolved

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -7,16 +7,16 @@ import { nanoid } from 'nanoid';
 import { useLocalStorage } from '../hooks/useLocalStorage';
 import { initailContacts } from '../utils/initialContacts';
 import { useSelector, useDispatch } from 'react-redux';
-import { add, remove } from '../redux/contacts/slice';
+import { add, remove, filter } from '../redux/contacts/slice';
 
 const STORAGE_KEY = 'contacts';
 
 export const App = () => {
   const dispatch = useDispatch();
-  const value = useSelector(state => state.contacts);
+  const contacts = useSelector(state => state.contacts.contacts);
 
-  const [contacts, setContacts] = useLocalStorage(STORAGE_KEY, initailContacts);
-  const [filter, setFilter] = useState('');
+  // const [contacts, setContacts] = useLocalStorage(STORAGE_KEY, initailContacts);
+  // const [filter, setFilter] = useState('');
 
   function formSubmitHandler({ name, number }) {
     const checkName = contacts.some(item =>
@@ -24,41 +24,44 @@ export const App = () => {
     );
     checkName
       ? alert(`${name} is already in contacts`)
-      : setContacts([{ id: nanoid(), name, number }, ...contacts]);
+      : dispatch(add({ id: nanoid(), name, number }));
   }
 
-  function onFilterChange([value]) {
-    !value ? setFilter('') : setFilter(value);
-  }
+  // function onFilterChange([value]) {
+  //   !value ? setFilter('') : setFilter(value);
+  // }
 
-  const filteredContacts = useMemo(() => {
-    return contacts.filter(item => {
-      console.log('item', item);
-      return item.name
-        .toLowerCase()
-        .trim()
-        .includes(filter.toLowerCase().trim());
-    });
-  }, [contacts, filter]);
+  // const filteredContacts = useMemo(() => {
+  //   return contacts.filter(item => {
+  //     return item.name
+  //       .toLowerCase()
+  //       .trim()
+  //       .includes(filter.toLowerCase().trim());
+  //   });
+  // }, [contacts]);
+
+  const filteredContacts = contacts;
 
   function deleteItem(itemID) {
-    setContacts(contacts.filter(item => item.id !== itemID));
+    dispatch(remove(itemID));
   }
 
   return (
     <Box width={1} p={4} bg="bgBasic" as="main">
-      <>
+      {/* <>
         <button onClick={() => dispatch(add(payload))}>Add</button>
         {value}
         <button onClick={() => dispatch(remove(payload))}>Remove</button>
-      </>
-
+      </> */}
       <h1>Phonebook</h1>
       <ContactForm onFormSubmit={formSubmitHandler} />
-
       <h2>Contacts</h2>
-      <Filter onChange={onFilterChange} />
+      {/* <Filter onChange={onFilterChange} /> */}
       <ContactList onDelete={deleteItem} list={filteredContacts} />
     </Box>
   );
 };
+
+{
+  /* <ContactList onDelete={deleteItem} list={filteredContacts} />; */
+}
--- src/redux/contacts/slice.js
+++ src/redux/contacts/slice.js
@@ -7,12 +7,19 @@ export const contactsSlice = createSlice({
   initialState,
   reducers: {
     add(state, action) {
-      state.push(action.payload);
+      state.contacts.push(action.payload);
     },
     remove(state, action) {
-      return state.filter(item => item.id !== action.payload);
+      // return state.contacts.filter(item => item.id !== action.payload);
+      const index = state.contacts.findIndex(
+        item => item.id === action.payload
+      );
+      state.contacts.splice(index, 1);
     },
+    // filter(state, action) {
+    //   state.contacts.filter();
+    // },
   },
 });
 
-export const { add, remove } = contactsSlice.actions;
+export const { add, remove, filter } = contactsSlice.actions;
